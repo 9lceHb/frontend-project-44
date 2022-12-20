db7bed971ab2e34bf0b3dcfa0965a8bc60649d4c
sergei <yasen20008@gmail.com>
2022-12-20 15:50:55 +0300

    step3

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const askName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+export default askName;
